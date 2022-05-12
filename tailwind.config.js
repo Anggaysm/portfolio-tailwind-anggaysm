2a12f94b4ab83dfaebccb99ee1184fafeeef84c0
Anggaysm <prasetiaangga28@gmail.com>
2022-05-12 22:18:33 +0700

    portfolio with tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      fontFamily: {
+        rubik: ["Rubik", "sans-serif"],
+      },
+      colors: {
+        primary: "#301CFF",
+        secondary: "#64748b",
+        dark: "#0f172a",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
